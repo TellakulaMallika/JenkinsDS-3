e93590fc255aa495476b8108378ba067234f304e
T.Mallika <tellakulamallika@gmailc.om>
2024-02-15 15:00:40 -0700

    firstcommit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Mallika");
+Console.WriteLine("Swapping of two numbers");
+int x = 10;int y = 20;
+x = x + y;
+y = x - y;
+x = x - y;
+
+Console.WriteLine("x: " + x);
+Console.WriteLine("y: " + y);
